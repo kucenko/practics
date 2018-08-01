ccb5fee6078924b5ee867e859f77e80cc31497c1
kucenko <o.kucenko@andersensoft.ru>
2018-08-01 21:37:07 +0300

    Client web socket

--- client/chat/src/App.js
+++ client/chat/src/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {connectWs, addWsListener, removeWsListener} from "./utils/ws";
+
+const {emit} = connectWs('ws://192.168.110.49:3000');
+
+addWsListener("connected_new_user", (data) => {
+  console.log(data);
+})
+
+addWsListener("disconnected_user", (data) => {
+  console.log(data);
+})
+
+addWsListener("message", (data) => {
+  console.log(data);
+});
+
+setTimeout(() => {
+  emit(`User: ${+new Date}`)
+}, 1000)
+
+setTimeout(() => {
+  emit(`Message: ${+new Date}`)
+}, 3000)
+
+
 class App extends Component {
   render() {
     return (
